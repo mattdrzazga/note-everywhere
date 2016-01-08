a617b463b15ac6be4894ced61e8f06f5eeb3f525
Mateusz Drzazga <matt.drzazga@gmail.com>
2016-01-08 19:46:21 +0100

    Minor change.

--- src/c++/pathresolver.cpp
+++ src/c++/pathresolver.cpp
@@ -10,6 +10,9 @@ PathResolver::PathResolver(QObject *parent) : QObject(parent)
 
 QString PathResolver::iconPath(const QString &iconName) const
 {
+#ifdef Q_OS_ANDROID
+    return "";
+#endif
 #ifdef Q_OS_LINUX
     QString possiblePath = "qrc:/linux/images/linux/" + iconName + ".%1";
     return possiblePath.arg("png");
